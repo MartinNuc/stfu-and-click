7759a0c72382a857f9bb5879bd196241a685e704
Martin Nuc <martin@nuc.cz>
2020-04-17 21:07:44 +0200

    Add logger for server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
-import Leaderboard from './routes/leaderboard';
-import Click from './routes/click';
+import morgan from 'morgan';
+
+import logger, { LoggerStream } from './utils/logger';
+
+import LeaderboardRoute from './routes/leaderboard';
+import ClickRoute from './routes/click';
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use('/api/v1/leaderboard', Leaderboard);
-app.use('/api/v1/click', Click);
+app.use(morgan('tiny', { stream: new LoggerStream() }));
 
-app.listen(process.env.PORT || 3000);
+app.use('/api/v1/leaderboard', LeaderboardRoute);
+app.use('/api/v1/click', ClickRoute);
+
+app.listen(port, () => logger.info(`Listening on port ${port}.`));
---
+++ server/src/utils/logger.ts
@@ -0,0 +1,24 @@
+import winston, { format, transports } from 'winston';
+import { StreamOptions } from 'morgan';
+
+const logger = winston.createLogger({
+  format: format.combine(
+    format.cli(),
+    format.colorize(),
+  ),
+  transports: [
+    new transports.Console({
+      level: 'debug',
+      handleExceptions: true,
+    }),
+  ],
+  exitOnError: false
+});
+
+export class LoggerStream implements StreamOptions {
+  write(message: string) {
+    logger.info(message.trim());
+  }
+}
+
+export default logger;
