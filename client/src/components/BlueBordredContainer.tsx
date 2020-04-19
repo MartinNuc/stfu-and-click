9b348079d9cea4141560b9ef09f5bbcc6a8cc196
Martin Nuc <martin@nuc.cz>
2020-04-20 00:56:25 +0200

    Create blue bordered component

---
+++ client/src/components/BlueBordredContainer.tsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+import styled from 'styled-components';
+
+export const BlueBorderedContainer = styled.div`
+  border-color: ${props => props.theme.primary};
+  border-style: solid;
+  border-radius: 0.5rem;
+  border-width: 0.25rem;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
