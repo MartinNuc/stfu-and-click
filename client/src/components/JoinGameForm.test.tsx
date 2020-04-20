c7bd3983fb25018911462b5a85ddb58f638118e5
Martin Nuc <martin@nuc.cz>
2020-04-20 16:03:07 +0200

    Create form for joining the game

---
+++ client/src/components/JoinGameForm.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, fireEvent, waitForDomChange } from '@testing-library/react';
+import { JoinGameForm } from './JoinGameForm';
+
+test(`it should emit team's name when submitted`, async () => {
+  const onJoin = jest.fn();
+  const { getByTestId } = render(<JoinGameForm onJoin={onJoin} />);
+  const input = getByTestId('team');
+  const submitButton = getByTestId('submit');
+  fireEvent.change(input, {target: { value: 'Bubaci'}});
+  fireEvent.click(submitButton);
+  expect(onJoin).toHaveBeenCalledWith('Bubaci');
+});
+
+test(`it should not allow empty team name`, async () => {
+  const onJoin = jest.fn();
+  const { getByTestId } = render(<JoinGameForm onJoin={onJoin} />);
+  const input = getByTestId('team');
+  const submitButton = getByTestId('submit');
+  fireEvent.change(input, {target: { value: ''}});
+  fireEvent.click(submitButton);
+  expect(getByTestId('error')).not.toBeNull();
+  expect(onJoin).not.toHaveBeenCalled();
+});
+
+test(`it should not display error right away`, async () => {
+  const { queryByTestId } = render(<JoinGameForm onJoin={() => {}} />);
+  expect(queryByTestId('error')).toBeNull();
+});
---
+++ client/src/components/JoinGameForm.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useState, FormEvent } from 'react';
+import styled from 'styled-components';
+import { Button } from 'atoms/Button';
+import { Input } from 'atoms/Input';
+import { Label } from 'atoms/Label';
+
+type Props = {
+  onJoin: (team: string) => void;
+};
+
+export const JoinGameForm: FC<Props> = ({ onJoin }) => {
+  const [team, setTeam] = useState('');
+  const [error, setError] = useState<null | string>(null);
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (!team) {
+      setError('Come up with a cool team name first');
+      return 
+    }
+    onJoin(team);
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label htmlFor="team">Enter your team name:</Label>
+        <Input
+          type="text"
+          id="team"
+          placeholder="Your mom"
+          data-testid="team"
+          onChange={(event) => setTeam(event.target.value)}
+        ></Input>
+        {error && <Error data-testid="error">{error}</Error>}
+      </FormGroup>
+      <Button data-testid="submit">Click!</Button>
+    </Form>
+  );
+};
+
+const Form = styled.form`
+  display: flex;
+  margin: 0.5rem;
+`;
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  margin: 0.5rem;
+`;
+
+const Error = styled.div`
+  color: red;
+  margin-top: 0.2rem;
+  font-size: small;
+`;
