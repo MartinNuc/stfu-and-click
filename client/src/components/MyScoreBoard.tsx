9b9844a00c1b0632415445df6ef60f4987f82fbe
Martin Nuc <martin@nuc.cz>
2020-04-21 17:31:38 +0200

    Style my score board component

--- client/src/components/MyScoreBoard.tsx
+++ client/src/components/MyScoreBoard.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import styled from 'styled-components';
 import { useSelector } from 'react-redux'
 import { RootState } from 'store';
 import { delimitThousands } from 'utils/thousands-delimiter';
@@ -7,10 +8,10 @@ export const MyScoreBoard = () => {
   const { myClicks, myTeam } = useSelector((state: RootState) => state.game)
   const myTeamClicks = 653335;
   return (
-    <div>
-      <BoardItem title="YourClicks" value={myClicks} />
-      <BoardItem title="Team clicks" value={myTeamClicks} />
-    </div>
+    <Container>
+      <BoardItem title="YourClicks:" value={myClicks} />
+      <BoardItem title="Team clicks:" value={myTeamClicks} />
+    </Container>
   )
 }
 
@@ -18,6 +19,29 @@ type BoardItemProps = {
   title: string;
   value: number;
 }
-const BoardItem: FC<BoardItemProps> = ({title, value}) => <div>
-  {title} {delimitThousands(value)}
-</div>
+const BoardItem: FC<BoardItemProps> = ({title, value}) => <Tile>
+  <Title>{title}</Title>
+  <Value>{delimitThousands(value)}</Value>
+</Tile>
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin: 2rem 0;
+`;
+
+const Title = styled.div`
+  font-style: italic;
+`;
+
+const Value = styled.div`
+  color: ${({theme}) => theme.primary};
+  font-size: 2.5rem;
+  font-weight: bold;
+`;
+
+const Tile = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
