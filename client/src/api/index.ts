cda4ac46282eab279481a25c4a8ba55aa2c46d94
Martin Nuc <martin@nuc.cz>
2020-04-21 02:30:26 +0200

    Use axios for data fetching

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,12 +1,13 @@
 import { ClickRequest, ClickResponse } from 'stfu-and-click-shared/src/click';
 import { LeaderboardEntry } from 'stfu-and-click-shared/src/leaderboard-entry';
+import axios from 'axios';
 
 export async function click(request: ClickRequest): Promise<ClickResponse> {
-  const response = await fetch('/api/v1/click');
-  return response.json();
+  const response = await axios.post('/api/v1/click', request);
+  return response.data;
 }
 
 export async function fetchLeaderboard(): Promise<LeaderboardEntry[]> {
-  const response = await fetch('/api/v1/leaderboard');
-  return response.json();
+  const response = await axios.get('/api/v1/leaderboard');
+  return response.data;
 }
